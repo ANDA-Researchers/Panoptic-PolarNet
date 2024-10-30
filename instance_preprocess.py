0add2a5e92ea766c140611ad33d3cbef22c92878
vuong-viet-hung <vuongviethung156@gmail.com>
2024-10-30 14:15:24 +0900

    Add `config_path` argument

--- instance_preprocess.py
+++ instance_preprocess.py
@@ -8,9 +8,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='')
     parser.add_argument('-d', '--data_path', default='data')
     parser.add_argument('-o', '--out_path', default='data')
+    parser.add_argument('-c', '--config_path', default='semantic-kitti.yaml')
 
     args = parser.parse_args()
 
-    train_pt_dataset = SemKITTI(args.data_path + '/sequences/', imageset = 'train', return_ref = True)
+    train_pt_dataset = SemKITTI(args.data_path + '/sequences/', imageset = 'train', return_ref = True, config_path=args.config_path)
     train_pt_dataset.save_instance(args.out_path)
     print('instance preprocessing finished.')
